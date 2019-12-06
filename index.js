b451190da86e9e2af11645a497d632ed92fbe465
Dan Selman <danscode@selman.org>
2019-12-06 13:34:43 +0000

    (feat) externalize query
    Signed-off-by: Dan Selman <danscode@selman.org>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jsonata = require('jsonata');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,32 +11,9 @@ const port = 3000
 app.post('/', (req, res) => {
     console.log('+++++++++');
     console.log(JSON.stringify(req.body, null, 4));
-    const expression = jsonata(`{
-        "$class": "io.clause.revenueshare.MonthlySalesReport",
-        "sales" : [
-         $map($.data, function($v, $i, $a) {
-               {
-                "$class" : "io.clause.revenueshare.Sale",
-                "transactionId" : "0",
-                "timestamp" : $v[0],
-                "product" : {
-                    "$class" : "io.clause.revenueshare.Product",
-                    "name" : $v[1],
-                    "listPrice" : {
-                        "$class": "org.accordproject.money.MonetaryAmount",
-                        "doubleValue": 200.0,
-                        "currencyCode": "USD"
-                    }
-                },
-                "price" : {
-                    "$class": "org.accordproject.money.MonetaryAmount",
-                    "doubleValue": $number(($v[2]),
-                    "currencyCode": "USD"
-                }
-            }
-    })
-        ]
-    }`);
+    const query = fs.readFileSync('jsonata.json').toString();
+    console.log(query);
+    const expression = jsonata(query);
     const result = expression.evaluate(req.body);
     console.log(result);
     res.send(result)
